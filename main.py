669a4d80323c885c0cfcac06ec431983780d51be
Fritz <fritz.reyes11@gmail.com>
2024-09-30 03:04:43 -0500

    reto completado

--- main.py
+++ main.py
@@ -1,78 +1,111 @@
-#Tarea 1: Convertir Strings en Minusculas
-
-#Tarea 2: Frecuencia de cada palabra en una cadena determinada
-
-#Tarea 3: Frecuencia de una palabra especifica 
-
-#Tare Extra: El analizador recibe un nombre de archivo y procesa el contenido del mismo, tiene un Metodo
-#            que permite escribir la frecuencia de palabras en otro archivo.
-
-# VER VIDEO DEL TUTORIAL
-
-import time
-
-# PASO 1 : DEFINIR EL STRING
-
-input = """La inteligencia artificial (IA) es un campo de estudio que abarca desde la simulación del razonamiento humano hasta la creación de máquinas que pueden aprender y tomar decisiones. A lo largo de los años, la IA ha avanzado significativamente, transformando industrias como la medicina, el transporte y la educación.
-
-El aprendizaje automático, una rama de la IA, permite que las computadoras desarrollen habilidades sin estar explícitamente programadas para ello. En lugar de seguir instrucciones específicas, las máquinas pueden analizar grandes cantidades de datos y detectar patrones para mejorar su rendimiento.
-
-Sin embargo, con estos avances también surgen preocupaciones. La ética en la inteligencia artificial es un tema que se discute ampliamente, ya que las decisiones automatizadas pueden afectar la privacidad y los derechos de las personas. A medida que la IA continúa desarrollándose, es fundamental establecer regulaciones y principios éticos que guíen su implementación.
-
-Por otro lado, el impacto positivo de la IA es innegable. Desde diagnósticos médicos más rápidos y precisos hasta vehículos autónomos que prometen reducir los accidentes de tránsito, las aplicaciones de la IA son diversas y están en constante expansión. En el futuro, se espera que la inteligencia artificial siga revolucionando la manera en que vivimos y trabajamos, brindando soluciones a problemas complejos que hasta hace poco parecían imposibles de resolver."""
-
-
-
-def replaceFromList(text, repList = []):
-  newText = ''
-  repSet = set(repList)
-  
-  for char in text:
-    if char not in repSet:
-      newText = newText + char
-
-  return newText
-
-# PASO 2: DEFINIR LA CLASE Y SUS ATRIBUTOS
 class TextAnalyzer():
-  
-  def __init__(self, text = ''):
-    # formattedText = text.replace('.', '').replace('!','').replace('?','').replace(',','')
-    formattedText = replaceFromList(text, ['.', ',', '!', '¡', '¿', '?', ';'])
 
-    # PASO 3: PASAR EL STRING A MINÚSCULAS    
-    self.fmtText = formattedText.lower()
-    print (self.fmtText)
-  
+    def __init__(self):               
+        self.fmt_text = ""
+        self.wordList = []    
 
-  # Paso 4: Contar la frecuencia de todas las palabras unicas
-  def freqAll(self):
+    # Metodo Estatico para extraer texto de un archivo
+    @staticmethod
+    def extract_text (filename):
+        try:
+            file = open(filename, encoding="utf-8")
 
-    #Partir cada vez que encuentre un espacio
-    wordList = self.fmtText.split(' ')
+            extracted_text = file.read()
+            return extracted_text
+        
+            file.close()
+        except Exception as error:
+            print ("Error al Extraer: ", error)
 
-    freqMap = {}
-    for word in set(wordList):
-      freqMap[word] = wordList.count(word)
+    @staticmethod
+    def insert_text (wordlist, filename):
 
-    return freqMap
+        with open(filename, 'w', encoding="utf-8") as outputFile:
+            outputFile.write("[")
+            for word in wordlist:
+                outputFile.write(f"'{word}', ")                       
+            outputFile.write("]")                  
 
+    # Metodo para procesar un texto, filtrando palabras determinadas
+    def process_text (self, raw_text, stop_words):
 
-  # PASO 5: CONTAR LA FRECUENCIA DE UNA PALABRA EN ESPECÍFICO
-  def freqOf(self, word):
+        #1. Convertir todo el texto a minusculas
+        formatted_text = raw_text.lower()
 
-    freqDict = self.freqAll()
+        #2.1 Eliminar saltos de linea, los convierte a espacio porque sino se pega a la otra letra                        
+        formatted_text = formatted_text.replace("\n"," ")
 
-    if word in freqDict:
-      return freqDict[word]
-    else:
-      return 0
+        #2.2 Eliminar caracteres que no sean letras +
+        #3. Eliminar cualquier digito presente en el texto
+        caracteresNoLetras = "1234567890.,;:¿?¡!()"
+        formatted_text = ''.join([char for char in formatted_text if char not in caracteresNoLetras])
+        
+        self.fmt_text = formatted_text
 
+        print("Raw Text: ", self.fmt_text , "\n") 
 
-###########__Main__########################
-analyzed = TextAnalyzer(input)
+        #4. Tokenizar el texto (convertirlo en una lista de palabras)
+        #Separa el texto por cada espacio    
+        wordList = self.fmt_text.split(" ")
+        #Borra los espacios vacios ingresados en la lista
+        while "" in wordList:
+            wordList.remove("")      
+        print("Tokenized Text: ", wordList, "\n")
 
-freqOfAllWords = analyzed.freqAll()
+        #5. Remover las stopwords mas comunes en español
+        #Se crea una lista de las StopWord, para esto se separa el texto
+        stop_wordList = stop_words.split("\n")
+        #Se crea una Lista Auxiliar para guardar las palabras adminitidas
+        wordSetFiltered =[]
+        
+        #Solo se agrega las palabas permitidas a la lista Auxiliar
+        for word in wordList:                        
+            if word not in stop_wordList:                
+                wordSetFiltered.append(word)        
+        
+        self.wordList = wordSetFiltered
+        print ("Filtrado:" , self.wordList , "\n") 
+    
+    
+    #Paso 4: Contral la frecuencia de cada palabra
+    def frequency_all(self):
+        
+        #Se inicializa el mapa de frecuencias
+        frequency_map ={}        
 
-print(freqOfAllWords)
-print(f"Occurrences of 'inteligencia': {analyzed.freqOf('inteligencia')}")
+        print("WordSet:", set(self.wordList), "\n")
+
+        for word in set(self.wordList):
+            frequency_map[word] = self.wordList.count(word)        
+        return frequency_map
+
+    # Paso 5: contar la frecuencia de una sola palaba
+    def frequency_of_word(self, word):
+        frequeny_map = self.frequency_all()
+        if word in frequeny_map:
+            return frequeny_map[word]
+        else:
+            return 0
+        
+
+########################################################################
+# 1. Abrir el archivo raw-text.txt y extraer el texto a Analizar.
+raw_text = TextAnalyzer.extract_text("raw-text.txt")
+
+# 2. Abrir el archivo stop-word.txt y extraer el texto a Filtrar.
+stop_words = TextAnalyzer.extract_text("stop-words.txt")
+
+# 3. Procesar el texto, como resultado da una lista de palabras que se guarda en el archivo processe-text.txt
+analized_text = TextAnalyzer()
+analized_text.process_text (raw_text, stop_words)
+
+# 4. Inserta el texto en el archivo "processed-text.txt"
+TextAnalyzer.insert_text (analized_text.wordList, 'processed-text.txt')
+
+# 5. Contrar la frecuencia de todas slas palabras en el texto
+frequencyAllWords = analized_text.frequency_all()
+print ("Frequency All Word", frequencyAllWords)
+
+# 6. Contrar la frecuencia de una palaba determinada
+word = "inteligencia"
+print (f"Ocurrence of {word}': '{ analized_text.frequency_of_word(word)}")
